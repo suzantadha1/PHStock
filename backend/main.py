20648a1c9ae8f4eaa74a274cde5b4fa3e212527b
Suzan Tadha <suzzantadha@gmail.com>
2026-05-10 12:01:10 -0500

    fix CORS url

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://phstock.vercel.app"
+        "https://ph-stock.vercel.app"
         ],
     allow_credentials=True,
     allow_methods=["*"],
